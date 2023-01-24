82c175e7c16f15583dc9a273c7b9e2d208d1973e
Kriti <kritib2502@gmail.com>
2023-01-23 21:26:25 -0800

    uploading images

---
+++ database.js
@@ -0,0 +1,51 @@
+const mysql = require("mysql2")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+  })
+  .promise()
+
+async function getImages() {
+  let query = `
+  SELECT * 
+  FROM images
+  ORDER BY created DESC
+  `
+
+  const [rows] = await pool.query(query);
+  return rows
+}
+exports.getImages = getImages
+
+async function getImage(id) {
+  let query = `
+  SELECT * 
+  FROM images
+  WHERE id = ?
+  `
+
+  const [rows] = await pool.query(query, [id]);
+  const result = rows[0];
+  return result
+}
+exports.getImage = getImage
+
+async function addImage(filePath, description) {
+  let query = `
+  INSERT INTO images (file_path, description)
+  VALUES(?, ?)
+  `
+
+  const [result] = await pool.query(query, [filePath, description]);
+  const id = result.insertId
+
+  return await getImage(id)
+}
+exports.addImage = addImage
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const fs = require('fs')
+// const db = require('./database')
+
+// 1
+const multer = require('multer')
+
+const app = express()
+// 2
+const upload = multer({ dest: 'images/' })
+
+app.use(express.static("build"));
+// 3
+
+app.get('/api/images', async (req, res)=> {
+  console.log("getting images...")
+  const result = await db.getImages()
+  res.send(result)
+})
+
+
+app.post('/api/images', upload.single('image'), async (req, res) => {
+  // 4
+  const imageName = req.file.filename
+  const description = req.body.description
+
+  // Save this data to a database probably
+
+  const result = await db.addImage(imageName,description)
+  res.send(result)
+})
+
+
+
+app.get('/images/:imageName', (req, res) => {
+  // do a bunch of if statements to make sure the user is 
+  // authorized to view this image, then
+
+  const imageName = req.params.imageName
+  const readStream = fs.createReadStream(`images/${imageName}`)
+  readStream.pipe(res)
+})
+
+app.get('*', (req, res) => {
+    //  res.sendFile('dist/index.html');
+      res.sendFile('build/index.html', { root: '.' });
+    
+    });
+
+app.listen(8080, () => console.log("listening on port 8080"))
