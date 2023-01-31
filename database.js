9dd6fa01c4e4920758e8d16e1a4b56a63de0321f
Kriti <kritib2502@gmail.com>
2023-01-31 11:24:04 -0800

    url

--- database.js
+++ database.js
@@ -9,6 +9,8 @@ const pool = mysql
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
+    url: process.env.MSQL_URL
   })
   .promise()
 
